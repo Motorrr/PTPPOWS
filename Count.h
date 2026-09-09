237f25f8d42389320ddcaa0a403cdf14f693ed6c
Motorrr <m2400@bk.ru>
2026-09-09 13:58:18 +0300

    Добавлена функция индексации слов

--- Count.h
+++ Count.h
@@ -1,30 +1,94 @@
-#pragma once
 #ifndef COUNT_H
 #define COUNT_H
 
-#include <fstream>   
-#include <iostream>  
-#include <map>      
-#include <string>    
-using namespace std;   
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <cctype>   
+using namespace std;
+
 map<string, int> countUniqueWords(const string& filename) 
 {
     map<string, int> wordCount;
     ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Ошибка: не удалось открыть файл " << filename << '\n';
+        return wordCount;
+    }
+    string word;
+    while (file >> word) 
+    {                  
+        ++wordCount[word];              
+    }
+    return wordCount;
+}
 
+vector<pair<string, vector<int>>> getWordPositions(const string& filename) 
+{
+    map<string, vector<int>> positions;       
+    vector<string> order;                 
+    ifstream file(filename);
     if (!file.is_open()) 
     {
         cerr << "Ошибка: не удалось открыть файл " << filename << '\n';
-        return wordCount;      
+        return {};       
     }
 
-    string word;
-    while (file >> word) 
-    {
-        ++wordCount[word];      
-    }
+    string currentWord;       int wordIndex = 0;    
+    char ch;
+    while (file.get(ch)) 
+    {                             
+        unsigned char uc = static_cast<unsigned char>(ch);           
+        if (isalnum(uc)) 
+        {   
+            currentWord.push_back(ch);
+        }
+        else 
+        {
+            if (!currentWord.empty()) 
+            {                                        
+                string lowerWord;
+                for (char c : currentWord) 
+                {
+                    lowerWord.push_back(tolower(static_cast<unsigned char>(c)));
+                }
+                
+                if (positions.find(lowerWord) == positions.end()) 
+                {
+                    order.push_back(lowerWord);
+                }
+                                
+                positions[lowerWord].push_back(wordIndex);
 
-    return wordCount;
+                currentWord.clear();                   
+                ++wordIndex;                       
+            }
+                    
+        }
+    }
+        if (!currentWord.empty()) 
+        {
+            string lowerWord;
+            for (char c : currentWord) 
+            {
+                lowerWord.push_back(tolower(static_cast<unsigned char>(c)));
+            }
+            if (positions.find(lowerWord) == positions.end()) 
+            {
+                order.push_back(lowerWord);
+            }
+            
+            positions[lowerWord].push_back(wordIndex); 
+        }
+        
+        vector<pair<string, vector<int>>> result;
+        for (const string& w : order) 
+        {
+            result.push_back({ w, positions[w] });
+        }
+    return result;
 }
 
 #endif
